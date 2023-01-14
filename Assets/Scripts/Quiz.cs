38622d043dce4fb56b09028151dde9dd23812f96
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-01-14 14:12:57 +0100

    feat: add Quiz.cs and code to emplementing questions into game

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    void Start()
+    {
+        questionText.text = question.GetQuestion();
+    }
+}
